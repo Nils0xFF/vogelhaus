9770e490beb09758aeed3074d128a69958f2dbb9
Nils0xFF <mail@ngeschwin.de>
2018-12-30 14:51:45 +0100

    changed shell to bash

--- RPi/setup/audio_setup.sh
+++ RPi/setup/audio_setup.sh
@@ -16,7 +16,7 @@ if [ ! -f /var/run/resume-after-reboot ]; then
   
     # add this script to zsh so it gets triggered immediately after reboot
     # change it to .bashrc if using bash shell
-    echo "$script" >> ~/.zshrc 
+    echo "$script" >> ~/.bashrc 
   
     # create a flag file to check if we are resuming from reboot.
     sudo touch /var/run/resume-after-reboot
@@ -29,7 +29,7 @@ else
     echo "resuming script after reboot.."
   
     # Remove the line that we added in zshrc
-    sed -i '/bash/d' ~/.zshrc 
+    sed -i '/bash/d' ~/.bashrc 
   
     # remove the temporary file that we created to check for reboot
     sudo rm -f /var/run/resume-after-reboot
@@ -37,7 +37,7 @@ else
     # continue with rest of the script
     ./audio_setup.exp
     rm i2samp.sh
-    
+
     # reboot again
     sudo reboot
 fi
