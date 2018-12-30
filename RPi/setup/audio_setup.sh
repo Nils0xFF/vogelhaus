d30c549004e201b01dab21fb0e78c6c3b7b05799
Nils0xFF <mail@ngeschwin.de>
2018-12-30 16:33:43 +0100

    changed path after reboot

--- RPi/setup/audio_setup.sh
+++ RPi/setup/audio_setup.sh
@@ -20,7 +20,7 @@ if [ ! -f "$MYPATH/setup/audio_setup_reboot" ]; then
     # add this script to bashrc so it gets triggered immediately after reboot
     echo "$script" >> ~/.bashrc 
     # create a flag file to check if we are resuming from reboot.
-    sudo touch "$MYPATH/audio_setup_reboot"
+    sudo touch "$MYPATH/setup/audio_setup_reboot"
     echo "rebooting.."
     # sudo reboot
 
@@ -30,7 +30,7 @@ else
     # Remove the line that we added in zshrc
     sed -i '/bash/d' ~/.bashrc 
     # remove the temporary file that we created to check for reboot
-    sudo rm -f "$MYPATH/audio_setup_reboot"
+    sudo rm -f "$MYPATH/setup/audio_setup_reboot"
     # Audio Setup part 2
     expect "$MYPATH/setup/audio_setup.exp" 
     rm "$MYPATH/setup/i2samp.sh"
