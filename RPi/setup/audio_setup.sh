f30ba03d7cebc3070f543d000ada5b74672c7ee1
Nils0xFF <mail@ngeschwin.de>
2018-12-30 15:21:17 +0100

    lowered timeout in exp script and added absoulte path to setup script

--- RPi/setup/audio_setup.sh
+++ RPi/setup/audio_setup.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 # filename: audio_setup.sh
 
+path = "/home/pi/vogelhaus/RPi"
+
 # check if the reboot flag file exists. 
 # We created this file before rebooting.
-if [ ! -f audio_setup_reboot ]; then
+if [ ! -f $path/setup/audio_setup_reboot ]; then
     echo "running script for the first time.."
-  
+
     # run your scripts here
-    curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh >> i2samp.sh
-    chmod u+x i2samp.sh
-    chmod u+x audio_setup.exp
-    ./audio_setup.exp
+    curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh >> $path/setup/i2samp.sh
+    chmod u+x $path/setup/i2samp.sh
+    chmod u+x $path/setup/audio_setup.exp
+    ./$path/setup/audio_setup.exp
     # Preparation for reboot
-    script="bash $PWD/audio_setup.sh"
+    script="bash $path/setup/audio_setup.sh"
   
     # add this script to zsh so it gets triggered immediately after reboot
     # change it to .bashrc if using bash shell
     echo "$script" >> ~/.bashrc 
   
     # create a flag file to check if we are resuming from reboot.
-    sudo touch audio_setup_reboot
-  
+    sudo touch $path/audio_setup_reboot
+    echo "$PWD" >> $path/audio_setup_reboot
+
     echo "rebooting.."
     # reboot here
     sudo reboot
@@ -32,11 +35,11 @@ else
     sed -i '/bash/d' ~/.bashrc 
   
     # remove the temporary file that we created to check for reboot
-    sudo rm -f audio_setup_reboot
+    sudo rm -f $path/audio_setup_reboot
 
     # continue with rest of the script
-    ./audio_setup.exp
-    rm i2samp.sh
+    ./$path/setup/audio_setup.exp
+    rm $path/setup/i2samp.sh
 
     # reboot again
     sudo reboot
