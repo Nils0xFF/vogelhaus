3f7eddc3f989bebbdaff851875621bdc1c0513e5
Nils0xFF <mail@ngeschwin.de>
2018-12-30 16:08:20 +0100

    added comments

--- RPi/setup/audio_setup.sh
+++ RPi/setup/audio_setup.sh
@@ -7,23 +7,21 @@ MYPATH="/home/pi/vogelhaus/RPi"
 if [ ! -f "$MYPATH/setup/audio_setup_reboot" ]; then
     echo "running script for the first time.."
 
-    # run your scripts here
+    # RTC Setup
+    # Audio Setup
     curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh >> "$MYPATH/setup/i2samp.sh"
     chmod u+x "$MYPATH/setup/i2samp.sh"
     chmod u+x "$MYPATH/setup/audio_setup.exp"
     expect "$MYPATH/setup/audio_setup.exp" $MYPATH
+
     # Preparation for reboot
     script="bash $MYPATH/setup/audio_setup.sh"
   
-    # add this script to zsh so it gets triggered immediately after reboot
-    # change it to .bashrc if using bash shell
+    # add this script to bashrc so it gets triggered immediately after reboot
     echo "$script" >> ~/.bashrc 
-  
     # create a flag file to check if we are resuming from reboot.
     sudo touch "$MYPATH/audio_setup_reboot"
-
     echo "rebooting.."
-    # reboot here
     # sudo reboot
 
 else 
@@ -31,15 +29,11 @@ else
   
     # Remove the line that we added in zshrc
     sed -i '/bash/d' ~/.bashrc 
-  
     # remove the temporary file that we created to check for reboot
     sudo rm -f "$MYPATH/audio_setup_reboot"
-
-    # continue with rest of the script
+    # Audio Setup part 2
     expect "$MYPATH/setup/audio_setup.exp" $MYPATH
     rm "$MYPATH/setup/i2samp.sh"
-
-    # reboot again
     # sudo reboot
 fi
 
