24ce3d27d014ccb1f38ab92d9329ee3a7fb66b91
Nils0xFF <mail@ngeschwin.de>
2018-12-31 14:01:40 +0100

    removed git from initial setup

--- RPi/setup/initial_setup.sh
+++ RPi/setup/initial_setup.sh
@@ -4,5 +4,4 @@ sudo apt-get -y upgrade
 sudo apt-get install -y libsdl1.2-dev
 sudo apt-get install -y expect
 sudo apt-get install -y python-smbus i2c-tools
-sudo apt-get install -y git
 sudo apt-get install -y python-pygame
